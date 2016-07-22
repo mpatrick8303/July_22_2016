2c4486d1c24d6e32a5b3b3b331976beab013aad5
Michael Patrick <mpatrick8303@gmail.com>
2016-07-22 14:19:42 -0400

    added the optional project

--- src/com/week1/day5/assignment1/MethodsAndFlow.java
+++ src/com/week1/day5/assignment1/MethodsAndFlow.java
@@ -2,5 +2,41 @@ package com.week1.day5.assignment1;
 
 public class MethodsAndFlow
 {
+	
+	private String[] stringArray;
+	
+	
+	public MethodsAndFlow(String[] stringArray)
+	{
+		this.stringArray = stringArray;
+	}
 
+	
+	public void outputSubset(int startPosition, int numberOfValues)
+	{
+		if(stringArray.length >= (startPosition + numberOfValues))
+		{
+			for(int i=startPosition; i<(startPosition+numberOfValues);i++)
+			{
+				System.out.println(stringArray[i]);
+			}
+			System.out.println();
+		}
+		else
+		{
+			System.out.println("You are outside of the array");
+			System.out.println();
+		}
+	}
+	
+	public void outputAll()
+	{
+		for(int i=0;i<stringArray.length;i++)
+		{
+			System.out.println(stringArray[i]);
+		}
+	}
+	
+	
+	
 }
--- src/com/week1/day5/assignment1/TestMethodsAndFlow.java
+++ src/com/week1/day5/assignment1/TestMethodsAndFlow.java
@@ -2,5 +2,16 @@ package com.week1.day5.assignment1;
 
 public class TestMethodsAndFlow
 {
-
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+		
+		MethodsAndFlow data = new MethodsAndFlow(args);
+		
+		data.outputSubset(2,1);
+		data.outputAll();
+	}
 }
